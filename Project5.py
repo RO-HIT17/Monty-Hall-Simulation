29a902f6ee0eeeec4bed8a0e42dac34010f04da8
RO-HIT17 <138398459+RO-HIT17@users.noreply.github.com>
2023-07-30 13:51:30 +0530

    Simulation

---
+++ Project5.py
@@ -0,0 +1,28 @@
+import random
+def monty_hall(simulations):
+    without_switching=0
+    with_switching=0
+    for a in range(simulations):    
+        doors=["goat","goat","car"]
+        random.shuffle(doors)
+        choice=random.choice(doors)
+        if choice=="car":
+            without_switching+=1
+        lst2=[]
+        if choice=="goat":
+            num1=doors.index(choice)
+            num2=doors.index("car")
+            lst2.append(doors[num1])
+            lst2.append(doors[num2])
+            for b in lst2:
+                if b=="car":
+                    with_switching+=1
+    return without_switching,with_switching
+simlations=int(input("Enter the number of simulations:"))
+count_wins_without_switching,count_wins_with_switching=monty_hall(simlations)
+
+print("Total Simulations:",simlations)
+print("Count of wins without switching:",count_wins_without_switching)
+print("Probability of wins without switching:",count_wins_without_switching/simlations)
+print("Count of wins without switching:",count_wins_with_switching)
+print("Probability of wins without switching:",count_wins_with_switching/simlations)
---
+++ Project5_V2.py
@@ -0,0 +1,37 @@
+import random
+def wins_without_switching(simulations):
+    without_switching=0
+    for a in range(simulations):    
+        doors=["goat","goat","car"]
+        random.shuffle(doors)
+        choice=random.choice(doors)
+        if choice=="car":
+            without_switching+=1
+    return without_switching
+def wins_with_switching(simulations):
+    with_switching=0
+    for a in range(simulations):    
+        doors=["goat","goat","car"]
+        random.shuffle(doors)
+        choice=random.choice(doors)             
+        lst2=[]
+        if choice=="goat":
+            num1=doors.index(choice)
+            num2=doors.index("car")
+            lst2.append(doors[num1])
+            lst2.append(doors[num2])
+            for b in lst2:
+                if b=="car":
+                    with_switching+=1
+    return with_switching
+
+simlations=int(input("Enter the number of simulations:"))
+count_wins_without_switching=wins_without_switching(simlations)
+count_wins_with_switching=wins_with_switching(simlations)
+print("Total Simulations:",simlations)
+print("Count of wins without switching:",count_wins_without_switching)
+print("Count of wins without switching:",count_wins_with_switching)
+print("Probability of wins without switching:",count_wins_without_switching/simlations)
+print("Probability of wins without switching:",count_wins_with_switching/simlations)
+print("Percentage of wins without switching:",round((count_wins_without_switching/simlations)*100,0),"%")
+print("Percentage of wins without switching:",round((count_wins_with_switching/simlations)*100,0),"%")
